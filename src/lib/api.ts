a75602558e3c8eec628ac5ff54f758b7fa573d92
Debajyoti mandal <150023795+debu000199@users.noreply.github.com>
2026-03-17 18:03:39 +0530

    Switch to mock API and clear base URL
    Changed API to use mock data and updated base URL.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,9 @@
 import { mockApi } from "./mockApi";
 import type { Product } from "./types";
 
-const USE_MOCK_API = false;
-const API_BASE_URL = "https://eapi.devsubhadipbag.in";
+const USE_MOCK_API = true;
+// const API_BASE_URL = "https://eapi.devsubhadipbag.in";
+const API_BASE_URL = "";
 // const API_BASE_URL = "http://localhost:3030"
 
 export const api = {
